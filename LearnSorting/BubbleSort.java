63e952dab8042228ed07468354edc032cb018e3c
pipgainer <abhishekpowade@gmail.com>
2025-03-08 22:39:30 +0530

    sorting revision

--- LearnSorting/BubbleSort.java
+++ LearnSorting/BubbleSort.java
@@ -4,14 +4,15 @@ public class BubbleSort {
 
         for (int i = arr.length; i > 0; i--) {
             for (int j = 0; j < i - 1; j++) {
-                if(arr[j] > arr[j+1]) {
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
             }
         }
 
+        // Revision
         for (int i : arr) {
             System.out.println(i);
         }
